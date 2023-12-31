a4279f93f1de3de74f103eefd5e7ade1f4aa9a40
Lorenzo Pellegrini <lorenzo.pellegrini@intramind-srl.com>
2023-12-31 13:56:52 +0100

    feature(Installer): Translated in english successfull message

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-sudo chmod 0777 aggiorna
-sudo mv aggiorna /usr/bin/aggiorna
 #! /bin/bash
 
 : '
@@ -10,7 +8,22 @@ sudo mv aggiorna /usr/bin/aggiorna
 @description : This script moves the update.sh script into /usr/bin/ folder. If the user is not sudo, it displays an error message and exits the script.
 @instruction : Simply run this script as sudo.
 '
+
+# Check if the user has sudo rights
+if [[ $EUID -ne 0 ]]; then
+  echo "You do not have correct rights. Please run this script using sudo."
+  exit 1
+fi
+
+# Give permissions to the main script
+sudo chmod 0777 update.sh
+# Move the script into /usr/bin/ folder
+sudo mv update.sh /usr/bin/update
+
+# Clear the screen
 clear
-echo "\nOperazione Completata!"
-echo "\nOra puoi avviare lo script digitando il comando:"
-echo "aggiorna"
+
+# Output success message
+printf "Operation complete!"
+printf "\nNow you can execute the script by using:\n"
+printf "update\n"
