e63106a156d292a2bf2685af8a30cc3bd906af0c
Lorenzo Pellegrini <lorenzo.pellegrini@intramind-srl.com>
2023-12-31 13:55:42 +0100

    docs(Installer): Added script documentation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 sudo chmod 0777 aggiorna
 sudo mv aggiorna /usr/bin/aggiorna
+#! /bin/bash
+
+: '
+@file        : setup.sh
+@author      : Lorenzo Pellegrini <lorenzo.pellegrini@intramind-srl.com>
+@version     : 2.0
+@since       : 2023-12-31
+@description : This script moves the update.sh script into /usr/bin/ folder. If the user is not sudo, it displays an error message and exits the script.
+@instruction : Simply run this script as sudo.
+'
 clear
 echo "\nOperazione Completata!"
 echo "\nOra puoi avviare lo script digitando il comando:"
